d94332e71ffc0ba0a79c6c93a6dea1003780e6df
Austin Curtis <austinjohncurtis@gmail.com>
2013-11-29 07:49:17 -0500

    commented out paper clip bucketing for amazon s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Pinteresting::Application.configure do
   config.action_mailer.default_url_options = { :host => 'austinpinteresting.herokuapp.com' }
 
 #sets paperclip to upload images to amazon s3
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  #config.paperclip_defaults = {
+   # :storage => :s3,
+    #:s3_credentials => {
+     # :bucket => ENV['S3_BUCKET_NAME'],
+      #:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+     # :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+   # }
+ # }
 end
